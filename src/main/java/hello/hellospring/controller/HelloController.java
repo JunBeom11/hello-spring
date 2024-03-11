90533e409d1246ae9d2abe724b7e5fe476da3517
jjjbbbblll <leejun9989@gmail.com>
2024-03-11 23:15:28 +0900

    깃 테스트

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -11,7 +11,6 @@ public class HelloController {
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
-
         return "hello";
     }
 }
