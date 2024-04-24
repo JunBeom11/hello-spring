fef6cb8e0a8094c629f786d0c34e61a069d1c3c1
jbl <leejun212@naver.com>
2024-04-24 19:00:08 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -2,35 +2,33 @@ package hello.hellospring;
 
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
 
     @Bean
     public MemberService memberSerivce() {
-        return new MemberService(memoryMemberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memoryMemberRepository() {
+//    @Bean
+//    public MemberRepository memoryMemberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-
-        return new JpaMemberRepository(em);
-    }
+//
+//        return new JpaMemberRepository(em);
+//    }
 
 }
