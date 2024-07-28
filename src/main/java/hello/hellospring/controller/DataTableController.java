4438a54a2998ca3fcaad9f6eab8ad915e68495cc
jbl <leejun212@naver.com>
2024-07-29 00:29:35 +0900

    datatable 사용법2

--- src/main/java/hello/hellospring/controller/DataTableController.java
+++ src/main/java/hello/hellospring/controller/DataTableController.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,7 +21,22 @@ public class DataTableController {
         );
         model.addAttribute("users", users);
 
-        return "datatable";
+        return "datatable/datatable";
+    }
+
+    @GetMapping("/datatable2")
+    public String getDataTable2() {
+        return "datatable/datatable2";
+    }
+
+    @GetMapping("/api/users")
+    @ResponseBody
+    public List<User> getUsers() {
+        return Arrays.asList(
+                new User(1, "Alice", 111),
+                new User(2, "Bob", 50),
+                new User(3, "Charlie", 5)
+        );
     }
 
 }
